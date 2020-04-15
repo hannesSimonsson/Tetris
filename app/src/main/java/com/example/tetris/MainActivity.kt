287a68c18b8b26c4c23f3d9aa773b40ed5c250d4
Hannes <k.hannes.simonsson@gmail.com>
2020-04-16 00:22:38 +0200

    Added a crude menu

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -15,12 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun sendMessage(view: View) {
+ /*   fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
+        startActivity(intent)
+    }
+*/
+    fun playGame(view: View) {
+        val intent = Intent(this, GamePlatform::class.java)
         startActivity(intent)
     }
 }
